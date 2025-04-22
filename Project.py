b2e7400ba9e6b724bd604a56b8cd400fee6c5b52
Brian Kim <122340149+BFubumaK@users.noreply.github.com>
2025-04-21 20:32:06 -1000

    made changes

--- Project.py
+++ Project.py
@@ -1,6 +1,6 @@
 from xgboost import XGBRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -19,27 +19,30 @@ df['hour'] = pd.to_datetime(df['datetime']).dt.hour
 df['minute'] = pd.to_datetime(df['datetime']).dt.minute
 df['day'] = pd.to_datetime(df['datetime']).dt.day
 df['month'] = pd.to_datetime(df['datetime']).dt.month
-df['year'] = pd.to_datetime(df['datetime']).dt.year
+# df['year'] = pd.to_datetime(df['datetime']).dt.year
 
-df.drop('datetime', axis=1,inplace=True)
-
+df.to_csv('trimmed_data.csv')
 
 #print(df)
 
+df.set_index('datetime', inplace=True)
+
+# temp = df.drop('meter_reading', axis=1)
+
 X = df.drop('meter_reading', axis=1)
 y = df['meter_reading']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=42,shuffle=False)
 
 model = XGBRegressor(objective='reg:squarederror',n_estimators=100, max_depth=5,random_state=42)
 model.fit(X_train, y_train)
 preds = model.predict(X_test)
 mse = mean_squared_error(y_test, preds)
+print(r2_score(y_test, preds))
 
-plt.plot(preds, y_test.index)
-plt.show()
 
-df = pd.read_csv('uhm2023.csv')
+# plt.plot(y_test.index, preds)
+# plt.show()
 
-df = df[df['meter_name'].str.contains('admin_serv_1')]
+
 
 
